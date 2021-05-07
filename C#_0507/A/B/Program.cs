0b821691a751a69d750d82489e7341b4ac1cd8a2
Yuu <yy556023@gmail.com>
2021-05-07 13:24:48 +0800

    first

---
+++ C#_0507/A/B/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("buuuuuuuuuuruuuuuuuuuuu\nsad\t123");
+            Console.WriteLine('d');
+
+            Console.ReadLine();
+        }
+    }
+}
